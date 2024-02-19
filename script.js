c131ee1813c9c7dfd6558ee1bb3153bb66540ad9
fullstackkk <alexsmit17264@gmail.com>
2024-02-19 21:03:24 +0300

    update

---
+++ script.js
@@ -0,0 +1,96 @@
+
+// let str
+// const num = 56
+// const bool = true 
+
+// const student = {
+//     name:'Albert',
+//     surname:'Бабалян',
+//     lastname:'Кароевич',
+//     age:26,
+//     isMarried:true,
+//     children:{
+//         name:'Tigran',
+//         surname:'Бабалян',
+//         lastname:'Albertovich',
+//         age:1,
+//         isMarried:false,
+//     },
+//     sayName:()=>{
+//         console.log(this.name)
+//     }
+// }
+
+ 
+
+// Термины: «унарный», «бинарный», «операнд»
+
+// Сложение +,
+// Вычитание -,
+// Умножение *,
+// Деление /,
+// Взятие остатка от деления %,
+// Возведение в степень **.
+
+// console.log( 50 % 2)
+// console.log( 3**3)
+
+// let str = 2 - '45' + true
+
+// console.log(str)
+// let num = 4
+// // num = --num + 2
+// num = num-- + 2
+
+// console.log( num)
+ 
+
+// …	…	…
+// 15	унарный плюс	+
+// 15	унарный минус	-
+// 14	возведение в степень	**
+// 13	умножение	*
+// 13	деление	/
+// 12	сложение	+
+// 12	вычитание	-
+// …	…	…
+// 2	присваивание	=
+// …	…	…
+
+// let num1 = 42
+// let num2 = 42
+
+// const result = num1 > num2
+
+// console.log( result ?  'хорошо':'плохо' )
+
+// if(num1 > num2){
+//     console.log(` Число ${num1} больше числа ${num2}!`)
+// }else if(num1 < num2){
+//     console.log(` Число ${num1} меньше числа ${num2}!`)
+// }else if(num1 = num2){
+//     console.log(` Число ${num1} равно числу ${num2}!`)
+// }
+
+// const student2 = {}
+
+// if(true){
+//     console.log(` Число  `)
+// }
+
+// const age = 55
+
+// switch(age){
+//     case age < 26:
+//         console.log('Пользователь не достиг 26 лет')  
+//         break
+//     case 26:
+//         console.log('Пользователь достиг 26 лет')  
+//         break
+//     case age > 26 :
+//         console.log('Пользователю больше 26 лет')  
+//         break
+//     default:
+//         console.log('Получено невалидное значение!!!')  
+//         break 
+// }
